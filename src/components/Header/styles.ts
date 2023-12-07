8fe8832043e735946dda8a9033dcc24b4014ce38
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2023-12-07 15:26:18 -0300

    Criando componente Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled, { DefaultTheme } from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  margin-top: 20px;
+  width: 46px;
+  height: 55px;
+`;
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,11 +4,4 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }: { theme: DefaultTheme }) =>
     theme.COLORS.GRAY_700};
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
 `;
