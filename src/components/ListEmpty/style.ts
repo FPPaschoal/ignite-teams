31906a7d24cdd05f4e3047e2ec0f59a175e6dda9
Felipe Paschoal <112896945+FPPaschoal@users.noreply.github.com>
2023-12-08 16:01:36 -0300

    Adicionado ListEmpty

---
+++ src/components/ListEmpty/style.ts
@@ -0,0 +1,16 @@
+import styled, { DefaultTheme } from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Message = styled.Text`
+  text-align: center;
+
+  font-size: ${({ theme }: { theme: DefaultTheme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }: { theme: DefaultTheme }) =>
+    theme.FONT_FAMILY.REGULAR};
+  color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_300};
+`;
